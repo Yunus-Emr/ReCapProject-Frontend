e82bc900d9bef2847cc0dcac6a22558638d33ac8
YUNUS EMRE AKCA <2022212026@cumhuriyet.edu.tr>
2023-07-23 12:32:20 +0300

    17.gün

--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RentalResponseModule } from '../models/rentalResponseModule';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RentalService {
+  apiUrl = 'https://localhost:7233/api/rentals/getrentaldetails';
 
-  constructor() { }
+  constructor(private httpCLient: HttpClient) {}
+
+  getRentals(): Observable<RentalResponseModule> {
+    return this.httpCLient.get<RentalResponseModule>(this.apiUrl);
+  }
 }
